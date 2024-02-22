050087c9b2a6c03cba3274dc01dcebd3277b0cff
vanessabrazuna <brazuna.nessa@gmail.com>
2024-02-21 22:13:12 -0300

    feat: add Dialog component

--- src/app.tsx
+++ src/app.tsx
@@ -1,7 +1,8 @@
 import { useState } from 'react'
 import { keepPreviousData, useQuery } from '@tanstack/react-query'
 import { useSearchParams } from 'react-router-dom'
-import { Plus, Search, FileDown, MoreHorizontal, Filter } from 'lucide-react'
+import { Search, FileDown, MoreHorizontal, Filter, Plus } from 'lucide-react'
+import * as Dialog from '@radix-ui/react-dialog'
 
 import { Header } from './components/header'
 import { Tabs } from './components/tabs'
@@ -81,14 +82,37 @@ export function App() {
       <main className="max-w-6xl mx-auto space-y-5">
         <div className="flex items-center gap-3">
           <h1 className="text-xl font-bold">Tags</h1>
-          <Button variant="primary">
-            <Plus className="size-3" />
-            Create new
-          </Button>
+
+          <Dialog.Root>
+            <Dialog.Trigger asChild>
+              <Button variant="primary">
+                <Plus className="size-3" />
+                Create new
+              </Button>
+            </Dialog.Trigger>
+
+            <Dialog.Portal>
+              <Dialog.Overlay className="fixed inset-0 bg-black/70" />
+              <Dialog.Content className="fixed space-y-10 p-10 right-0 top-0 bottom-0 h-screen min-w-[320px] z-10 bg-zinc-950 border-l border-zinc-900">
+                <div className="space-y-3">
+                  <Dialog.Title className="text-xl font-bold">
+                    Create tag
+                  </Dialog.Title>
+                  <Dialog.Description className="text-sm text-zinc-500">
+                    Tags can be used to group videos about similar concepts.
+                  </Dialog.Description>
+                </div>
+
+                {/* <CreateTagForm /> */}
+              </Dialog.Content>
+            </Dialog.Portal>
+          </Dialog.Root>
+
+          {/* {} */}
         </div>
 
         <div className="flex items-center justify-between">
-          <div className="flex items-center">
+          <form onSubmit={handleFilter} className="flex items-center gap-2">
             <Input variant="filter">
               <Search className="size-3" />
               <Control
@@ -97,12 +121,11 @@ export function App() {
                 value={filter}
               />
             </Input>
-          </div>
-
-          <Button onClick={handleFilter}>
-            <Filter className="size-3" />
-            Filter
-          </Button>
+            <Button type="submit">
+              <Filter className="size-3" />
+              Apply filters
+            </Button>
+          </form>
 
           <Button>
             <FileDown className="size-3" />
